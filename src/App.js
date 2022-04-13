8f042244d705ff8c1fbe9a0e1caca954539136c8
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-04-13 20:55:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import  axios from 'axios';
+import {useState,useEffect} from 'react';
 import './App.css';
-
+import Search from './search'
 function App() {
+
+  const [categories,setCategories] = useState();
+  const [search,setSearch] = useState();
+  const [dataSearch,setDataSearch] = useState();
+
+
+  useEffect(() => {
+      retrieveCategories()
+  }, [])
+
+
+  const retrieveCategories = async () => {
+    try {
+      const {data}  = await axios.get("https://api.publicapis.org/categories")
+
+      // console.log(response.data, '<== response categories')
+      setCategories(data.categories)
+
+
+    } catch (error) {
+
+      console.log(error,'<==error retrive categories')
+      
+    }
+  }
+  
+  const handleChange = (e) => {
+    // console.log(e.target.value)
+    setSearch(e.target.value);
+
+  }
+  
+  const handleSubmit = async (e) => {
+    try {
+
+      // agar halaman tidak di refresh
+      e.preventDefault()
+
+      const {data} = await axios.get(`https://api.publicapis.org/entries?category=${search}`)
+      // console.log(data,'<== response handle submit')
+      setDataSearch(data.entries)
+    }
+     catch (error)
+     {
+      console.log(error,'<==error handle submit')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>React Api</h1>
+
+
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleChange}/>
+          <button type='submit'>Submit</button>
+        </form>
+ 
+        <ul>
+          {dataSearch && dataSearch.map((item,index) => {
+            console.log(item)
+            return (
+              <>
+                <li>{item.API}</li>
+                <li>{item.Category}</li>
+                <li><a href={item.Link}>Link</a></li>
+                <br />
+                <br />
+              </>
+            ) 
+          } )}
+        </ul> 
+
+
+        {/* <ul>
+          {categories && categories.map((category,index) => (
+              <li key={index}>{category}</li>
+            ))}
+        </ul> */}
     </div>
   );
 }
---
+++ src/search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function search(nama,link,kategori) {
+  return (
+      <>
+        <li>{nama}</li>
+        <li>{link}</li>
+        <li>{kategori}</li>
+        <br></br>
+        <br></br>
+      </>
+  )
+}
